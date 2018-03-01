2e98431a1b5c207fc022f9d850ee86e8e6bc5cdd
Harrissou Sant-anna <delazj@gmail.com>
2018-03-01 02:06:03 +0100

    Add Help toolbar to Toolbar menus

--- src/app/qgisapp.cpp
+++ src/app/qgisapp.cpp
@@ -2482,7 +2482,8 @@ void QgisApp::createToolBars()
                       << mVectorToolBar
                       << mDatabaseToolBar
                       << mWebToolBar
-                      << mLabelToolBar;
+                      << mLabelToolBar
+                      << mSnappingToolBar;
 
 
   mSnappingWidget = new QgsSnappingWidget( QgsProject::instance(), mMapCanvas, mSnappingToolBar );
