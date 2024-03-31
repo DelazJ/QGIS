8e94e2b336a38d98d4195b9ae86a8bb825e4ab4e
DelazJ <delazj@gmail.com>
2024-03-31 20:02:57 +0200

    un peu de cleanup dans la doc pyqgis

--- src/server/qgsserverogcapihandler.h
+++ src/server/qgsserverogcapihandler.h
@@ -56,34 +56,34 @@ class QgsServerApiContext;
  *
  * \code{.py}
  *
- * class Handler1(QgsServerOgcApiHandler):
- *   """Example handler"""
+ *   class Handler1(QgsServerOgcApiHandler):
+ *     """Example handler"""
  *
- *   def path(self):
+ *     def path(self):
  *       return QtCore.QRegularExpression("/handlerone")
  *
- *   def operationId(self):
+ *     def operationId(self):
  *       return "handlerOne"
  *
- *   def summary(self):
+ *     def summary(self):
  *       return "First of its name"
  *
- *   def description(self):
+ *     def description(self):
  *       return "The first handler ever"
  *
- *   def linkTitle(self):
+ *     def linkTitle(self):
  *       return "Handler One Link Title"
  *
- *   def linkType(self):
+ *     def linkType(self):
  *       return QgsServerOgcApi.data
  *
- *   def handleRequest(self, context):
+ *     def handleRequest(self, context):
  *       """Simple mirror: returns the parameters"""
  *
  *       params = self.values(context)
  *       self.write(params, context)
  *
- *   def parameters(self, context):
+ *     def parameters(self, context):
  *       return [QgsServerQueryStringParameter("value1", True, QgsServerQueryStringParameter.Type.Double, "a double value")]
  *
  *
